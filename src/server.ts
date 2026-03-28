9e8861596d6c6dd90b4ba6b753eadd7a11b40438
Nicho <nich4090@gamil.com>
2026-03-28 15:40:08 -0500

    feat(database): add PostgreSQL connection with dotenv configuration

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 import app from './app'; 
 import { config } from "./config/config";
-
+import './persistence/database';
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`);
 });
