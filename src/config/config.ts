5d55acec252f66ea9b79afab543beb6ec48e9291
Nicho <nich4090@gamil.com>
2026-03-28 16:44:28 -0500

    feat(config): implement singleton pattern for environment variables

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,48 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-dotenv.config();
+dotenv.config(); 
 
-export const config = {
-  PORT: process.env.PORT ?? 3000,
-};
+class Config {
+  private static instance: Config;
+
+
+  public readonly PORT: number;
+  public readonly DB_HOST: string;
+  public readonly DB_PORT: number;
+  public readonly DB_USER: string;
+  public readonly DB_PASSWORD: string;
+  public readonly DB_NAME: string;
+  public readonly JWT_SECRET: string;
+
+ 
+  private constructor() {
+    if (
+      !process.env.DB_HOST ||
+      !process.env.DB_PORT ||
+      !process.env.DB_USER ||
+      !process.env.DB_PASSWORD ||
+      !process.env.DB_NAME ||
+      !process.env.JWT_SECRET
+    ) {
+      throw new Error("Missing required environment variables");
+    }
+
+    this.PORT = Number(process.env.PORT ?? 3000);
+    this.DB_HOST = process.env.DB_HOST;
+    this.DB_PORT = Number(process.env.DB_PORT);
+    this.DB_USER = process.env.DB_USER;
+    this.DB_PASSWORD = process.env.DB_PASSWORD;
+    this.DB_NAME = process.env.DB_NAME;
+    this.JWT_SECRET = process.env.JWT_SECRET;
+  }
+
+  public static getInstance(): Config {
+    if (!Config.instance) {
+      Config.instance = new Config();
+    }
+    return Config.instance;
+  }
+}
+
+
+export const config = Config.getInstance();
--- src/persistence/database.ts
+++ src/persistence/database.ts
@@ -1,14 +1,13 @@
-import dotenv from 'dotenv';
 import { Pool } from 'pg';
+import { config } from "../config/config";
 
-dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: Number(process.env.DB_PORT),
+  user: config.DB_USER,
+  host: config.DB_HOST,
+  database: config.DB_NAME,
+  password: config.DB_PASSWORD,
+  port: config.DB_PORT,
 });
 
 
