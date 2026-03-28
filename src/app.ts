f9a1bf9a5957d4e1f02c2396dffa13ec7b5fa5d1
Nicho <nich4090@gamil.com>
2026-03-28 13:14:58 -0500

    chore: setup project structure, TypeScript config and Express server

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/ping', (_req: Request, res: Response) => {
+  res.send('pong');
+});
+
+export default app;
